3188a4b5ce0b2c2ffe21e3894fb415817e34a9a4
gel00 <gellaigabor@gmail.com>
2021-04-21 17:27:25 +0100

    params orimalisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import BeerList from './components/BeerList';
-import SearchBar from './components/SearchBar';
-import {getBeer} from './services/FetchBeer.js'
-import React, { useState } from 'react';
-import styles from './App.module.scss';
-import library from "./data/fa-library";
+import "./App.css";
+import BeerList from "./components/BeerList";
+import SearchBar from "./components/SearchBar";
+import { getBeer } from "./services/FetchBeer.js";
+import React, { useState } from "react";
 
 function App() {
   const [beerList, setBeerList] = useState([]);
-  
-  const searchEventHandler = (obj)=>{
-    console.log(obj);
-    getBeer(setBeerList,obj);
+
+  const searchEventHandler = (filtersObj) => {
+    getBeer(setBeerList, filtersObj);
   };
-  
-  
+
   return (
     <div className="App">
       <h1>Beer API</h1>
-      <SearchBar handler={searchEventHandler}/>
-      <BeerList list={beerList}/>
+      <SearchBar handler={searchEventHandler} />
+      <BeerList list={beerList} />
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,60 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "./SearchBar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Range from '../Range';
+import Range from "../Range";
 
-const SearchBar = ({handler}) => {
-  const [filters,setFilters] = useState({});
+const SearchBar = ({ handler }) => {
+  const [filters, setFilters] = useState({});
   const [name, setName] = useState("");
-  const getText= ({target})=> {
+  const getText = ({ target }) => {
     setName(target.value);
     handler({
-      alc : filters.alc,
-      color : filters.color,
-      name : target.value
+      alc: filters.alc,
+      color: filters.color,
+      name: target.value,
     });
   };
 
   const [isFiltersVisible, toggleFilterVisible] = useState(false);
-  const getFilters = (alc,color)=>{
+  const getFilters = (alc, color) => {
     setFilters({
       alc: alc,
-      color: color
+      color: color,
     });
     handler({
-      alc : alc,
-      color : color,
-      name : name
+      alc: alc,
+      color: color,
+      name: name,
     });
   };
 
-  const toggleFilters = ()=>{
+  const toggleFilters = () => {
     toggleFilterVisible(!isFiltersVisible);
+    //Clear filters when closed
     if (!isFiltersVisible) {
       setFilters({});
     }
+    //Fetch new data without any filter
     handler({
-      alc : "",
-      color : "",
-      name : name
+      alc: "",
+      color: "",
+      name: name,
     });
   };
   return (
     <div className={styles["search-bar"]}>
       <div className={styles["search-field"]}>
-        <label htmlFor="search"><FontAwesomeIcon icon="search" /></label>
-        <input id="beer" placeholder="name of the beer" type="text" onChange={getText} autoFocus={true} autoComplete={"off"}/>
-        <button onClick={toggleFilters}><FontAwesomeIcon icon="sliders-h" /></button>
+        <label htmlFor="search">
+          <FontAwesomeIcon icon="search" />
+        </label>
+        <input
+          id="beer"
+          placeholder="name of the beer"
+          type="text"
+          onChange={getText}
+          autoFocus={true}
+          autoComplete={"off"}
+        />
+        <button onClick={toggleFilters}>
+          <FontAwesomeIcon icon="sliders-h" />
+        </button>
       </div>
-      {
-        isFiltersVisible && <div className={styles.filter}>
-        <Range getFilters={getFilters}/>
-      </div>
-      }
-      
+      {isFiltersVisible && (
+        <div className={styles.filter}>
+          <Range getFilters={getFilters} />
+        </div>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
